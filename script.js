19d69866b050e0ad1d3c5936785254b8f17de6e0
fernandezsaezmanuela <fernandezsaezmanuela@gmail.com>
2026-04-08 16:32:07 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll (".link-menu");
-links.forEach (link => {
-    link.addEventListener("click", function(){
-        links.forEach(l => l.classList.remove("activo"));
-        this.classList.add("activo");
-    });
-});
